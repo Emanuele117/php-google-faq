d9c52d82af4112c431f6a60890d46a88a142cb85
Emanuele117 <emanuele.pica7@gmail.com>
2021-12-03 15:59:33 +0100

    FAQ inserite con php

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+$faq = [
+    [
+        "domanda" => "Come state attuando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) sul diritto all'oblio?",
+        "risposta" => "La recente sentenza della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. Il tribunale ha rilevato che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere i risultati per le query che includono il nome della persona. Per qualificarsi, i risultati mostrati dovrebbero essere inadeguati, irrilevanti, non più rilevanti o eccessivi.
+        Da quando questa sentenza è stata pubblicata il 13 maggio 2014, abbiamo lavorato senza sosta per conformarvisi. Questo è un processo complicato perché dobbiamo valutare ogni singola richiesta e bilanciare i diritti dell'individuo di controllare i propri dati personali con il diritto del pubblico di conoscere e distribuire informazioni.
+        Se hai una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma che abbiamo ricevuto la tua richiesta. Valuteremo quindi il tuo caso: tieni presente che potrebbe volerci del tempo perché abbiamo già ricevuto molte di queste richieste. Nel valutare la tua richiesta, esamineremo se i risultati includono informazioni obsolete sulla tua vita privata. Esamineremo anche se c'è un interesse pubblico nelle informazioni rimanenti nei nostri risultati di ricerca, ad esempio se si riferiscono a truffe finanziarie, negligenza professionale, condanne penali o alla tua condotta pubblica come funzionario governativo (eletto o non eletto). Questi sono giudizi difficili e, in quanto organizzazione privata, potremmo non essere in una buona posizione per decidere sul tuo caso. Se non sei d'accordo con la nostra decisione, puoi contattare il tuo DPA locale.
+        Non vediamo l'ora di lavorare a stretto contatto con le autorità per la protezione dei dati e altri nei prossimi mesi mentre perfezioniamo il nostro approccio. La sentenza della CGUE costituisce un cambiamento significativo per i motori di ricerca. Sebbene siamo preoccupati per il suo impatto, riteniamo anche importante rispettare la sentenza della Corte e stiamo lavorando duramente per elaborare un processo conforme alla legge.
+        Quando cerchi un nome, potresti vedere un avviso che dice che i risultati potrebbero essere stati modificati in conformità con la legge sulla protezione dei dati in Europa. Mostriamo questo avviso in Europa quando un utente cerca la maggior parte dei nomi, non solo le pagine che sono state interessate da una rimozione."
+    ],
+    [
+        "domanda" => "In che modo Google protegge la mia privacy e protegge le mie informazioni?",
+        "risposta" => "Sappiamo che la sicurezza e la privacy sono importanti per te, e lo sono anche per noi. Per noi è una priorità fornire una forte sicurezza e darti la certezza che le tue informazioni siano sicure e accessibili quando ne hai bisogno.
+
+        Lavoriamo costantemente per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno in sicurezza e impieghiamo esperti di fama mondiale nella sicurezza dei dati per proteggere le tue informazioni. Abbiamo anche creato strumenti per la privacy e la sicurezza di facile utilizzo come Google Dashboard, la verifica in due passaggi e le impostazioni degli annunci. Quindi, quando si tratta delle informazioni che condividi con Google, hai il controllo.
+        
+        Puoi saperne di più su sicurezza e protezione online, incluso come proteggere te stesso e la tua famiglia online, nel Centro per la sicurezza di Google .
+        
+        Scopri di più su come manteniamo le tue informazioni personali private e al sicuro e ti diamo il controllo."
+    ],
+    [
+        "domanda" => "Come posso rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        "risposta" => "I risultati di ricerca di Google riflettono i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi la rimozione dei risultati di ricerca da Google non rimuoverà i contenuti dal web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui è pubblicato il contenuto e chiedergli di apportare una modifica. Inoltre, se in base alla legge europea sulla protezione dei dati desideri richiedere la rimozione di determinate informazioni su di te che appaiono nei risultati di ricerca di Google, fai clic qui . Una volta che il contenuto è stato rimosso e Google ha preso nota dell'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. Se hai una richiesta di rimozione urgente, puoi anchevisita la nostra pagina di aiuto per ulteriori informazioni ."
+    ],
+    [
+        "domanda" => "Le mie query di ricerca vengono inviate ai siti web quando faccio clic sui risultati della Ricerca Google?",
+        "risposta" => "In alcuni casi sì. Quando fai clic su un risultato di ricerca in Ricerca Google, il tuo browser web può anche inviare l'indirizzo Internet, o URL, della pagina dei risultati di ricerca alla pagina web di destinazione come URL di riferimento . L'URL della pagina dei risultati di ricerca a volte può contenere la query di ricerca che hai inserito. Se stai utilizzando la ricerca SSL (la funzionalità di ricerca crittografata di Google), nella maggior parte dei casi, i tuoi termini di ricerca non verranno inviati come parte dell'URL nell'URL di riferimento. Esistono alcune eccezioni a questo comportamento, ad esempio se si utilizzano alcuni browser meno diffusi. Maggiori informazioni sulla ricerca SSL possono essere trovate qui. Le query di ricerca o le informazioni contenute nell'URL di riferimento possono essere disponibili tramite Google Analytics o un'interfaccia di programmazione dell'applicazione (API). Inoltre, gli inserzionisti possono ricevere informazioni relative alle parole chiave esatte che hanno attivato un clic sull'annuncio."
+    ],
+
+]
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <?php
+        foreach ($faq as $key => $value) {
+        ?> <h2> <?php echo $value["domanda"] ?></h2> 
+        <p><?php echo $value["risposta"] ?></p> <?php
+        }
+        ?>
+    </main>
+</body>
+
+</html>
